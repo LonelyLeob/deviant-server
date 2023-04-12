b0a9eb9913863aa3e759d9cb7dc3302433e5937a
LonelyLeob <l0nelyleob@gmail.com>
2023-04-12 13:03:28 +0100

    add geo in girlserializer

--- girl/serializers.py
+++ girl/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.gis.geoip2 import GeoIP2
 from .models import Girl, Link, Avatar
 
 class LinkSerializer(serializers.ModelSerializer):
@@ -16,7 +17,19 @@ class AvatarSerializer(serializers.ModelSerializer):
 class GirlSerializer(serializers.ModelSerializer):
     links = LinkSerializer(many=True)
     avatars = AvatarSerializer(many=True)
+    geo = serializers.SerializerMethodField("_geo")
 
     class Meta:
         model = Girl
-        fields = ['avatars', 'nickname', 'additional_info',  'links']
+        fields = ['avatars', 'nickname', 'additional_info',  'links', 'geo']
+
+    def _geo(self, _: Girl):
+        x_forwarded_for = self.context['request'].META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = self.context['request'].META.get('REMOTE_ADDR')
+        geocoder = GeoIP2()
+        # if ip == "127.0.0.1":
+        #     return "Moscow"
+        return geocoder.city(ip)['city']
