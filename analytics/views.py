bc8efc17d5aad45cb6dc2fb98e9dd16af2c70663
LonelyLeob <l0nelyleob@gmail.com>
2023-04-14 23:55:06 +0100

    add additional logic to utm

--- analytics/views.py
+++ analytics/views.py
@@ -4,14 +4,23 @@ from .models import Mark, Source
 def set_utm(request: HttpRequest):
     app = request.GET.get('app')
     if app:
-        source = Source.objects.get(shortcut=app)
-        Mark.objects.create(app=source).save()
+        if Source.objects.filter(shortcut=app).exists():
+            source = Source.objects.get(shortcut=app)
+            Mark.objects.create(app=source).save()
+        else:
+            if not Source.objects.filter(name='Не опознано').exists():
+                source = Source.objects.create(name='Не опознано', description='Пользователь подключился из неопознанного источника', shortcut='-')
+                source.save()
+                Mark.objects.create(app=source).save()
+            else:
+                source = Source.objects.filter(name='Не опознано')
+                Mark.objects.create(app=source).save()
     else:
-        if not Source.objects.filter(name='Другое').exists():
+        if not Source.objects.filter(name='Напрямую').exists():
             source = Source.objects.create(name='Напрямую', description='Пользователь подключился напрямую', shortcut='-')
             source.save()
             Mark.objects.create(app=source).save()
         else:
-            source = Source.objects.filter(name='Другое')
+            source = Source.objects.filter(name='Напрямую')
             Mark.objects.create(app=source).save()
     return HttpResponseRedirect("https://yankawildy.fun")
