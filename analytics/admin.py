aa5269f6349e6cc77502b01751cf8202a86a9c99
LonelyLeob <l0nelyleob@gmail.com>
2023-04-15 23:41:21 +0100

    add another countries to the board

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Geo, Mark, Source
 from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models import Count
+from django.db.models import Count, Sum
 import json
+from itertools import chain
 
 @admin.register(Geo)
 class GeoAdmin(admin.ModelAdmin):
@@ -10,10 +11,15 @@ class GeoAdmin(admin.ModelAdmin):
     change_list_template = 'change_list_geo.html'
 
     def changelist_view(self, request, extra_context=None):
+        pre_count = 1/5*Geo.objects.all().count()
         chart_data = (
-            Geo.objects.values('country').annotate(requests=Count('country')).filter(requests__gte=1/4*Geo.objects.all().count())
+            chain(
+            Geo.objects.values('country').annotate(requests=Count('country')).filter(requests__gte=pre_count),
+            [sum(geo['requests'] for geo in Geo.objects.values('country').annotate(requests=Count('country')).filter(requests__lt=pre_count))]
+            )
         )
         as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        print(as_json)
         extra_context = extra_context or {"chart_data": as_json}
         return super().changelist_view(request, extra_context)
 
