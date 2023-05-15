76848783ce1730e83d99bc6076ef79ac806d207c
LonelyLeob <l0nelyleob@gmail.com>
2023-05-15 14:36:59 +0300

    add logging girl in middleware

--- analytics/middleware/geo.py
+++ analytics/middleware/geo.py
@@ -1,15 +1,18 @@
 from django.http import HttpRequest
 from django.contrib.gis.geoip2 import GeoIP2
 from ..models import GeoCounter
+from girl.models import Girl
 from .simple import SimpleMiddleware
 
 class GeoMiddleware(SimpleMiddleware):
     def __call__(self, request: HttpRequest):
         ip = self._process_ip(request)
         if ip and ip != "127.0.0.1":
+            origin = request.headers.get('Origin')
+            girl = Girl.objects.get(domain=origin)
             geocoder = GeoIP2()
             country = geocoder.country_name(ip)
-            counter, _ = GeoCounter.objects.get_or_create(country=country)
+            counter, _ = GeoCounter.objects.get_or_create(country=country, girl=girl)
             counter.requests_counter+=1
             counter.save()
         return self._get_response(request)
--- analytics/middleware/utm.py
+++ analytics/middleware/utm.py
@@ -1,5 +1,6 @@
 from django.core.handlers.wsgi import WSGIRequest
 from ..models import Source, Mark
+from girl.models import Girl
 from .simple import SimpleMiddleware
 
 
@@ -7,42 +8,47 @@ class UTMMiddleware(SimpleMiddleware):
     def __call__(self, request: WSGIRequest):
         response = self._get_response(request)
         app = request.GET.get('app')
-        host = request.headers.get('Host')
-        if not host:
+        origin = request.headers.get('Origin')
+        if "admin" in request.get_full_path():
+            return response
+        if not origin:
             return response
         if app:
             if Source.objects.filter(shortcut=app).exists():
                 source = Source.objects.get(shortcut=app)
-                mark, _ = Mark.objects.get_or_create(app=source)
+                girl = Girl.objects.get(domain=origin)
+                mark, _ = Mark.objects.get_or_create(app=source, girl=girl)
                 mark.requests_counter+=1
                 mark.save()
             else:
                 if not Source.objects.filter(name='Не опознано').exists():
+                    girl = Girl.objects.get(domain=origin)
                     source = Source.objects.create(name='Не опознано',
                                                 description='Пользователь подключился из неопознанного источника',
-                                                shortcut='-')
+                                                shortcut='-',)
                     source.save()
-                    mark, _ = Mark.objects.get_or_create(app=source)
+                    mark, _ = Mark.objects.get_or_create(app=source, girl=girl)
                     mark.requests_counter+=1
                     mark.save()
                 else:
+                    girl = Girl.objects.get(domain=origin)
                     source = Source.objects.get(name='Не опознано')
-                    mark, _ = Mark.objects.get_or_create(app=source)
+                    mark, _ = Mark.objects.get_or_create(app=source, girl=girl)
                     mark.requests_counter+=1
                     mark.save()
         else:
-            if "admin" in request.get_full_path():
-                return response
+            girl = Girl.objects.get(domain=origin)
             if not Source.objects.filter(name='Напрямую').exists():
                 source = Source.objects.create(name='Напрямую', description='Пользователь подключился напрямую',
                                             shortcut='-')
                 source.save()
-                mark, _ = Mark.objects.get_or_create(app=source)
+                mark, _ = Mark.objects.get_or_create(app=source, girl=girl)
                 mark.requests_counter+=1
                 mark.save()
             else:
+                girl = Girl.objects.get(domain=origin)
                 source = Source.objects.get(name='Напрямую')
-                mark, _ = Mark.objects.get_or_create(app=source)
+                mark, _ = Mark.objects.get_or_create(app=source, girl=girl)
                 mark.requests_counter+=1
                 mark.save()
         return response
