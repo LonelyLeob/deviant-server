e2afd1f99570dd25927aa5855dfc3f5d5e6b50bf
LonelyLeob <l0nelyleob@gmail.com>
2023-06-27 16:10:05 +0300

    upd search fields

--- analytics/admin.py
+++ analytics/admin.py
@@ -5,20 +5,20 @@ from .models import GeoCounter, Mark, Source, Guest
 @admin.register(Guest)
 class GuestAdmin(admin.ModelAdmin):
     list_display = ['ip', 'visited_at']
-    search_fields = ['girl',]
+    search_fields = ['girl__name', 'girl__domain']
     list_filter = ['girl',]
 
 @admin.register(GeoCounter)
 class GeoAdmin(admin.ModelAdmin):
     list_display = ['country', 'requests_counter']
-    search_fields = ['girl',]
-    list_filter = ['girl',]
+    search_fields = ['girl__name', 'girl__domain']
+    list_filter = ['girl']
 
 
 @admin.register(Mark)
 class MarkAdmin(admin.ModelAdmin):
     list_display = ['app', 'requests_counter']
-    search_fields = ['girl',]
+    search_fields = ['girl__name', 'girl__domain']
     list_filter = ['girl',]
 
 
